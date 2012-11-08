a8b01bbe3d275aaa843799c5e77386de192d8eb8
dbc-apprentice <apprentice@devbootcamp.com>
2012-11-08 11:27:13 -0800

    basic twitter bootstrap applied

---
+++ pollster/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pollster::Application.config.secret_token = '03820b31bd50b1b1463e7591d0bf8a4229511f47af18e8d01aa06ad2d74d9fffedaaf6a6b18997104bb8386bafe37fe62335169298d6bf14d07e2fd48f4e6288'
